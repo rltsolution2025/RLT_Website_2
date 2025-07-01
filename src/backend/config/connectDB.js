4ac39ece58ce7a245349e0d4a1675b578f939122
rltsolution2025 <social.rltdigital@gmail.com>
2025-07-01 14:07:13 +0530

    mongodb cluster updated

--- src/backend/config/connectDB.js
+++ src/backend/config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/rlt-academy', {
+    await mongoose.connect('mongodb+srv://socialrltdigital:947yhRqQVrTd4VLr@rlt-tri-force.kdcchsc.mongodb.net/?retryWrites=true&w=majority&appName=RLT-TRI', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
